277bcb23377afcde076a88f7c32e21afaecd53e8
Uller <fullersted@gmail.com>
2025-10-22 18:36:51 +0200

    hehheh

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -13,6 +13,6 @@ VALUES ('PlayStation 5', 'Spillekonsol', 4000.00, null, null),
        ('GitHub CoPilot', 'Coding Assistant', 100.00, null, null),
        ('Ramme Coca Cola', 'Drikkevarer', 69.00, null, null);
 
--- 3. Wishlist
+-- 3. Wishlist hej
 INSERT INTO wishlist (wishlistName, description, ownerName, ownerId, wishes)
 VALUES ('Fødselsdagsønsker','Osmans ønskeliste til hans fødselsdag', 'Osman Git', 1,'GitHub CoPilot, PlayStation 5, Barbie');
