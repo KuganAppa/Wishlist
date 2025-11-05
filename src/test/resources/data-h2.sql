e9769f379d6806e6dfff227b51763f3ee33204df
Uller <fullersted@gmail.com>
2025-11-05 14:43:23 +0100

    FÆRDIG

--- src/test/resources/data-h2.sql
+++
@@ -1,8 +0,0 @@
-MERGE INTO users (userName, email, password, dateOfBirth) KEY(email)
-VALUES ('Osman Git', 'osman@github.com', 'osman123', '1993-10-21');
-
-MERGE INTO users (userName, email, password, dateOfBirth) KEY(email)
-VALUES ('Oskar Cola', 'oskar@cocacola.com', 'jegelskercola', '1953-05-03');
-
-MERGE INTO users (userName, email, password, dateOfBirth) KEY(email)
-VALUES ('Kugan Appa', 'kugan-appa@ceomail.com', 'skejs123', '1954-02-08');
