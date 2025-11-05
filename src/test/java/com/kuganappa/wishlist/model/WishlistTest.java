736dc506cfe702afe51b81588d0330435f774276
Uller <fullersted@gmail.com>
2025-11-05 14:23:47 +0100

    change3

--- src/test/java/com/kuganappa/wishlist/model/WishlistTest.java
+++ src/test/java/com/kuganappa/wishlist/model/WishlistTest.java
@@ -2,11 +2,12 @@ package com.kuganappa.wishlist.model;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 import java.util.List;
 import static org.junit.jupiter.api.Assertions.*;
 @SpringBootTest
-
+@ActiveProfiles("test")
 class WishlistTest {
 
     @Test
--- src/test/java/com/kuganappa/wishlist/repository/UserRepositoryIntegrationTest.java
+++ src/test/java/com/kuganappa/wishlist/repository/UserRepositoryIntegrationTest.java
@@ -5,12 +5,14 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 import java.time.LocalDate;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
+@ActiveProfiles("test")
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 class UserRepositoryIntegrationTest {
 
